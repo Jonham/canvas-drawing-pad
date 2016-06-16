7cd7f2645a96a9edb3772a79186fd48b6b848d30
Jonham <jonham@qq.com>
2016-06-16 23:06:33 +0800

    func: DrawArea calculate drawing area and create Blob object

--- js/main.js
+++ js/main.js
@@ -14,6 +14,66 @@ var resetCanvas = function(e) {
 $on(window, 'load', resetCanvas);
 $on(window, 'resize', resetCanvas);
 
+/* DrawArea
+ *   update( p{x,y} )   => undefined
+ *   calculate()        => {x,y,width,height}
+*/
+var DrawArea = {
+    minX: 1000, maxX: 0,
+    minY: 1000, maxY: 0,
+
+    update: function(p) {
+        var me = this;
+        me.minX = p.x < me.minX? p.x: me.minX;
+        me.maxX = p.x > me.maxX? p.x: me.maxX;
+
+        me.minY = p.y < me.minY? p.y: me.minY;
+        me.maxY = p.y > me.maxY? p.y: me.maxY;
+    },
+    calculate: function() {
+        var me = this;
+        return {
+            x: me.minX,
+            y: me.minY,
+            width: me.maxX - me.minX,
+            height: me.maxY - me.minY
+        };
+    }
+};
+
+var btnSave = document.querySelector('#btnSave');
+var btnReset = document.querySelector('#btnReset');
+
+$on(btnSave, 'click', function() {
+    var area = DrawArea.calculate();
+    var imageData = ctx.getImageData(area.x, area.y, area.width, area.height);
+
+    var iCanvas = document.createElement('canvas');
+    var iCtx = iCanvas.getContext('2d');
+    iCanvas.height = area.height;
+    iCanvas.width = area.width;
+    iCtx.putImageData(imageData,0,0);
+
+    jh = iCanvas.toBlob(function(blob) {
+        var newImg = document.createElement("img"),
+        url = URL.createObjectURL(blob);
+
+        newImg.onload = function() {
+            // no longer need to read the blob so it's revoked
+            URL.revokeObjectURL(url);
+        };
+
+        newImg.src = url;
+        newImg.download = "your_drawing.png";
+        document.body.appendChild(newImg);
+    });
+});
+$on(btnReset, 'click', function() {
+    ctx.clearRect(0,0, canvas.width,canvas.height);
+});
+
+
+
 var Pen = {
     ctx: ctx,
     temp: {}, // store temp data
@@ -71,6 +131,9 @@ var Pen = {
 
         Pen.ctx.beginPath();
         Pen.ctx.moveTo(p.x, p.y);
+
+        DrawArea.update( p );
+
         return p;
     },
     lineTo: function( e ) {
@@ -78,6 +141,8 @@ var Pen = {
 
         var w = Pen.calWidth(p);
         Pen.ctx.lineTo(p.x, p.y);
+
+        DrawArea.update( p );
 
         return p;
     },
